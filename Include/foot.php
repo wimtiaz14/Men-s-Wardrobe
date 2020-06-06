0b23088afadf8da9d96e7f1a216fb1c86e7fbaa4
wimtiaz14 <66513655+wimtiaz14@users.noreply.github.com>
2020-06-06 15:24:00 +0500

    Header And Footer For dynamic interface

---
+++ Include/foot.php
@@ -0,0 +1,42 @@
+ <footer class="mainfooter" role="contentinfo">
+        <div class="footer-middle">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3 col-sm-6">
+              <!--Column1-->
+              <div class="footer-pad">
+                <h4>Menu</h4>
+                <ul class="list-unstyled">
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="AboutUs.php">About Us</a></li>
+                  <li><a href="Products.php">Products</a></li>
+                  <li><a href="ContactUs.php">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+            
+            
+              <div class="col-md-3 offset-md-6">
+                  <h4>Follow Us</h4>
+                  <ul class="social-network social-circle">
+                    <li class="nav-item">
+                        <a href="https://www.facebook.com/pg/Mens-Wardrobe-1197038727111840/jobs/" target="_blank" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
+                        </li>
+                    <li class="nav-item"><a href="https://linkedin.com" class="icoLinkedin" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    <li class="nav-item"><a href="https://instagram.com/mens_wardrobe_115?igshid=4qg3jwsc34ek" target="_blank" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a>
+                        </li>
+                  </ul>				
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-md-12 copy">
+                  <p class="text-center">&copy; Copyright 2018 - Men's Wardrobe.  All rights reserved.</p>
+              </div>
+          </div>
+      
+      
+        </div>
+        </div>
+      </footer>
+      
---
+++ Include/header.php
@@ -0,0 +1,39 @@
+<header>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <a href="../index.php" class="navbar-brand">
+                <img src="../Assets/Images/logoo.jpeg" height="100" alt="CoolBrand">
+              
+            </a>
+            
+                    
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">                    
+                </li>                
+                <li class="nav-item">
+                    &nbsp <a href="https://www.facebook.com/pg/Mens-Wardrobe-1197038727111840/jobs/" target="_blank" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+                <li class="nav-item"><a href="https://linkedin.com" class="icoLinkedin" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+                <li class="nav-item"><a href="https://instagram.com/mens_wardrobe_115?igshid=4qg3jwsc34ek" target="_blank" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+              </ul>
+        </nav>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+           <div class="container">
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav">
+                    <a href="welcome.php" class="nav-item nav-link active">Home</a>
+                    <a href="addpage.php" class="nav-item nav-link">Add Product</a>
+                    <a href="view.php" class="nav-item nav-link">View product</a>
+                    <a href="../index.php" class="nav-item nav-link">Logout</a>
+					
+                </div>            
+            </div>
+           </div>
+        </nav>
+    </header>
